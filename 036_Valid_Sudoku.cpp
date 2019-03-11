5eac766cee158ce758666fc8bfee41aa5f9089f1
Wei Zhang <mercurialzhang@163.com>
2019-03-11 15:00:29 +0800

    fix

--- 036_Valid_Sudoku.cpp
+++ 036_Valid_Sudoku.cpp
@@ -1,12 +1,12 @@
 class Solution {
 public:
     bool isValidSudoku(vector<vector<char>>& board) {
-        int box3_3[9][10] = {0};
-        int box_ver[9][10] = {0};
-        int box_hor[9][10] = {0};
+        int box3_3[9][9] = {0};
+        int box_ver[9][9] = {0};
+        int box_hor[9][9] = {0};
         for (int i = 0; i < 9; i ++) {
             for (int j = 0; j < 9; j ++) {
-                int num = board[i][j] - '0';
+                int num = board[i][j] - '1';
                 if(board[i][j] != '.') {
                     if (box3_3[(i / 3) * 3 + (j / 3)][num]|| box_ver[j][num]|| box_hor[i][num]) {
                         return false;
