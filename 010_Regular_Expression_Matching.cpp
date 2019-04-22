08ec50823c498f5309f2b0bddbdeb4ffa4d3d36a
MercurialZhang <mercurialzhang@163.com>
2019-04-22 16:13:06 +0800

    add file

--- 010_Regular_Expression_Matching.cpp
+++ 010_Regular_Expression_Matching.cpp
@@ -2,23 +2,23 @@ class Solution {
 public:
     bool isMatch(string s, string p) {
         if (s.length() == 0){
-        if (p.length() & 1) return false;
-        else {
-            for (int i = 1; i < p.length(); i += 2) {
-                if (p[i] != '*') return false;
+            if (p.length() & 1) return false;
+            else {
+                for (int i = 1; i < p.length(); i += 2) {
+                    if (p[i] != '*') return false;
+                }
             }
+            return true;
         }
-        return true;
-    }
-    if (p.length() == 0) return false;
-    if (p.length() > 1 && p[1] == '*') {
-        if (p[0] == '.' || s[0] == p[0]) {
-            return isMatch(s.substr(1), p) || isMatch(s, p.substr(2));
-        } else return isMatch(s, p.substr(2));
-    } else {
-        if (p[0] == '.' || s[0] == p[0]) {
-            return isMatch(s.substr(1), p.substr(1));
-        } else return false;
-    }
+        if (p.length() == 0) return false;
+        if (p.length() > 1 && p[1] == '*') {
+            if (p[0] == '.' || s[0] == p[0]) {
+                return isMatch(s.substr(1), p) || isMatch(s, p.substr(2));
+            } else return isMatch(s, p.substr(2));
+        } else {
+            if (p[0] == '.' || s[0] == p[0]) {
+                return isMatch(s.substr(1), p.substr(1));
+            } else return false;
+        }
     }
 };
